bc4870d5c838bee542c6ebf44c4a52cf05b6322f
pipoquint-svg <pipoquint@gmail.com>
2026-08-25 08:54:56 -0300

    QA: scope FK index invariants to authoritative schema

--- supabase/tests/database/102_foreign_key_indexes.test.sql
+++ supabase/tests/database/102_foreign_key_indexes.test.sql
@@ -1,6 +1,6 @@
 begin;
 
-select plan(8);
+select plan(6);
 
 select ok(to_regclass('public.appointment_balance_collections_created_by_admin_idx') is not null,
   'appointment_balance_collections created_by_admin_id is indexed');
@@ -14,10 +14,6 @@ select ok(to_regclass('public.balance_collection_divergences_resolved_by_admin_i
   'balance_collection_divergences resolved_by_admin_id is indexed');
 select ok(to_regclass('public.customer_access_events_actor_admin_idx') is not null,
   'customer_access_events actor_admin_id is indexed');
-select ok(to_regclass('public.kommo_guard_outgoing_messages_schedule_idx') is not null,
-  'kommo_guard_outgoing_messages matched_schedule_id is indexed');
-select ok(to_regclass('public.kommo_guard_schedules_rule_idx') is not null,
-  'kommo_guard_schedules rule_id is indexed');
 
 select * from finish();
 rollback;
